3d6a24de67483bc5451a2a2ed207a7cecca1abe7
Alfred Klomp <git@alfredklomp.com>
2015-09-19 13:44:37 +0200

    pngloader.c: small style fixes

--- pngloader.c
+++ pngloader.c
@@ -44,9 +44,9 @@ malloc_fn (png_structp png_ptr, png_size_t size)
 		size += __BIGGEST_ALIGNMENT__;
 	}
 	// Heap should be big enough; check just in case:
-	if (((heap_head + size) - heap) > HEAP_SIZE) {
+	if (((heap_head + size) - heap) > HEAP_SIZE)
 		return NULL;
-	}
+
 	heap_head += size;
 	return (png_voidp)ret;
 }
@@ -71,10 +71,10 @@ user_read_data (png_structp png_ptr, png_bytep data, png_size_t length)
 }
 
 static void
-user_error_fn (png_structp png_ptr, png_const_charp error_msg)
+user_error_fn (png_structp png_ptr, png_const_charp msg)
 {
 	(void)png_ptr;
-	(void)error_msg;
+	(void)msg;
 
 	// According to the libpng docs, this function should not return control,
 	// but longjmp back to the caller:
@@ -83,10 +83,10 @@ user_error_fn (png_structp png_ptr, png_const_charp error_msg)
 }
 
 static void
-user_warning_fn (png_structp png_ptr, png_const_charp warning_msg)
+user_warning_fn (png_structp png_ptr, png_const_charp msg)
 {
 	(void)png_ptr;
-	(void)warning_msg;
+	(void)msg;
 
 	// According to the docs, the warning should simply return, not longjmp.
 	cancel_flag = true;
@@ -103,7 +103,7 @@ pngloader_on_init (void)
 }
 
 static bool
-check_is_png (void)
+is_png (void)
 {
 	char buf[8];
 
@@ -119,31 +119,43 @@ check_is_png (void)
 }
 
 static bool
-load_png_file (png_structp *ppng_ptr, png_infop *pinfo_ptr, unsigned int *height, unsigned int *width, void **rawbits)
+load_png_file (unsigned int *height, unsigned int *width, void **rawbits)
 {
-	png_structp png_ptr = *ppng_ptr;
-	png_infop info_ptr = *pinfo_ptr;
 	bool ret = false;
 
 	*rawbits = NULL;
 
 	// First file read happens here; this is probably also where we can
 	// expect the real io latency hit; check carefully for cancellation:
-	if (!check_is_png() || cancel_flag) {
+	if (!is_png())
 		return false;
-	}
-	if ((png_ptr = png_create_read_struct_2(PNG_LIBPNG_VER_STRING, NULL, user_error_fn, user_warning_fn, NULL, malloc_fn, free_fn)) == NULL) {
+
+	if (cancel_flag)
 		return false;
-	}
+
+	// Create read struct:
+	png_ptr = png_create_read_struct_2(
+		PNG_LIBPNG_VER_STRING,
+		NULL,
+		user_error_fn,
+		user_warning_fn,
+		NULL,
+		malloc_fn,
+		free_fn
+	);
+
+	if (png_ptr == NULL)
+		return false;
+
 	// Instantiate our own read function:
 	png_set_read_fn(png_ptr, NULL, user_read_data);
 
-	if ((info_ptr = png_create_info_struct(png_ptr)) == NULL) {
+	if ((info_ptr = png_create_info_struct(png_ptr)) == NULL)
 		goto exit_0;
-	}
-	if (setjmp(png_jmpbuf(png_ptr))) {
+
+	if (setjmp(png_jmpbuf(png_ptr)))
 		goto exit_0;
-	}
+
 	// Sane limits on image:
 	png_set_user_limits(png_ptr, 500, 500);
 
@@ -152,39 +164,39 @@ load_png_file (png_structp *ppng_ptr, png_infop *pinfo_ptr, unsigned int *height
 
 	// Transform input into 8-bit RGB:
 	{
-		png_byte color_type = png_get_color_type(png_ptr, info_ptr);
-		png_byte bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+		png_byte color_type = png_get_color_type (png_ptr, info_ptr);
+		png_byte bit_depth  = png_get_bit_depth  (png_ptr, info_ptr);
 
 		// Convert paletted images to RGB:
-		if (color_type == PNG_COLOR_TYPE_PALETTE) {
+		if (color_type == PNG_COLOR_TYPE_PALETTE)
 			png_set_palette_to_rgb(png_ptr);
-		}
+
 		// Upsample low-bit grayscale images to 8-bit:
-		if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8) {
+		if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
 			png_set_expand_gray_1_2_4_to_8(png_ptr);
-		}
+
 		// Upsample low-bit images to 8-bit:
-		if (bit_depth < 8) {
+		if (bit_depth < 8)
 			png_set_packing(png_ptr);
-		}
+
 		// Discard alpha channel:
-		if (color_type & PNG_COLOR_MASK_ALPHA) {
+		if (color_type & PNG_COLOR_MASK_ALPHA)
 			png_set_strip_alpha(png_ptr);
-		}
+
 		// Upsample grayscale to RGB:
 		if (color_type == PNG_COLOR_TYPE_GRAY
-		 || color_type == PNG_COLOR_TYPE_GRAY_ALPHA) {
+		 || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
 			png_set_gray_to_rgb(png_ptr);
-		}
+
 		png_read_update_info(png_ptr, info_ptr);
 	}
-	*width = png_get_image_width(png_ptr, info_ptr);
-	*height = png_get_image_height(png_ptr, info_ptr);
+	*width  = png_get_image_width  (png_ptr, info_ptr);
+	*height = png_get_image_height (png_ptr, info_ptr);
 
 	// Allocate the rawbits image data:
-	if ((*rawbits = malloc(*width * *height * 3)) == NULL) {
+	if ((*rawbits = malloc(*width * *height * 3)) == NULL)
 		goto exit_0;
-	}
+
 	// Allocate array of row pointers, read actual image data:
 	// TODO: check if it's wise to allocate this on the stack:
 	{
@@ -226,22 +238,22 @@ pngloader_main (void *data)
 	fd = -1;
 	cancel_flag = false;
 
-	if ((p->filename = tile_filename(p->req.zoom, p->req.x, p->req.y)) == NULL || cancel_flag) {
+	if ((p->filename = tile_filename(p->req.zoom, p->req.x, p->req.y)) == NULL || cancel_flag)
 		goto exit;
-	}
+
 	// Get a file descriptor to the file. We just want a file descriptor to
 	// associate with this thread so that we know what to clean up, and not
 	// wait for the disk to actually deliver, so we issue a nonblocking call:
-	if ((fd = open(p->filename, O_RDONLY | O_NONBLOCK)) < 0 || cancel_flag) {
+	if ((fd = open(p->filename, O_RDONLY | O_NONBLOCK)) < 0 || cancel_flag)
 		goto exit;
-	}
+
 	// Now that we have the fd, make it properly blocking:
-	if (fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) & ~O_NONBLOCK) < 0 || cancel_flag) {
+	if (fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) & ~O_NONBLOCK) < 0 || cancel_flag)
 		goto exit;
-	}
-	if (!load_png_file(&png_ptr, &info_ptr, &height, &width, &rawbits) || cancel_flag) {
+
+	if (!load_png_file(&height, &width, &rawbits) || cancel_flag)
 		goto exit;
-	}
+
 	if (fd >= 0) {
 		close(fd);
 		fd = -1;
@@ -266,7 +278,8 @@ exit:	if (fd >= 0) {
 void
 pngloader_on_cancel (void)
 {
-	if (!init_ok) return;
+	if (!init_ok)
+		return;
 
 	cancel_flag = true;
 }
@@ -274,7 +287,8 @@ pngloader_on_cancel (void)
 void
 pngloader_on_exit (void)
 {
-	if (!init_ok) return;
+	if (!init_ok)
+		return;
 
 	free(heap);
 }
