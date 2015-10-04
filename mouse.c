14b49203df1b4519c32b3bd2764406ea9b9be863
Alfred Klomp <git@alfredklomp.com>
2015-10-04 14:53:02 +0200

    mouse.c: rewrite code for finding event xy

--- mouse.c
+++ mouse.c
@@ -4,9 +4,6 @@
 #include "viewport.h"
 #include "autoscroll.h"
 
-#define EVENT_X		(int)(event->x)
-#define EVENT_Y		(int)(allocation.height - (int)event->y)
-
 static int button_dragged = false;
 static int button_pressed = false;
 static int button_pressed_x;
@@ -14,42 +11,53 @@ static int button_pressed_y;
 static int button_num;
 static int click_halted_autoscroll = 0;
 
-static GtkAllocation allocation;
+// Translate event coordinates to have origin in bottom left:
+// Use a macro and not an inline function because the basic GdkEvent
+// does not have x and y members:
+#define event_get_xy							\
+	int x, y;							\
+	do {								\
+		GtkAllocation allocation;				\
+									\
+		gtk_widget_get_allocation(widget, &allocation);		\
+		x = event->x;						\
+		y = allocation.height - event->y;			\
+	} while (0)
 
 void
 on_button_press (GtkWidget *widget, GdkEventButton *event)
 {
-	gtk_widget_get_allocation(widget, &allocation);
+	event_get_xy;
 
 	button_dragged = false;
 	button_pressed = true;
-	button_pressed_x = EVENT_X;
-	button_pressed_y = EVENT_Y;
+	button_pressed_x = x;
+	button_pressed_y = y;
 	button_num = event->button;
 
-	if (button_num == 1) {
-		viewport_hold_start(EVENT_X, EVENT_Y);
-	}
+	if (button_num == 1)
+		viewport_hold_start(x, y);
+
 	// Does the press of this button cause the autoscroll to halt?
 	click_halted_autoscroll = autoscroll_is_on();
 	autoscroll_stop();
-	autoscroll_measure_down(button_pressed_x, button_pressed_y);
+	autoscroll_measure_down(x, y);
 }
 
 void
 on_button_motion (GtkWidget *widget, GdkEventButton *event)
 {
-	gtk_widget_get_allocation(widget, &allocation);
+	event_get_xy;
 
-	int dx = EVENT_X - button_pressed_x;
-	int dy = EVENT_Y - button_pressed_y;
+	int dx = x - button_pressed_x;
+	int dy = y - button_pressed_y;
 
 	button_dragged = true;
 
 	// Left mouse button:
 	if (button_num == 1) {
-		autoscroll_measure_hold(EVENT_X, EVENT_Y);
-		viewport_hold_move(EVENT_X, EVENT_Y);
+		autoscroll_measure_hold(x, y);
+		viewport_hold_move(x, y);
 	}
 	// Right mouse button:
 	if (button_num == 3) {
@@ -57,8 +65,8 @@ on_button_motion (GtkWidget *widget, GdkEventButton *event)
 		viewport_tilt(dy);
 	}
 	gtk_widget_queue_draw(widget);
-	button_pressed_x = EVENT_X;
-	button_pressed_y = EVENT_Y;
+	button_pressed_x = x;
+	button_pressed_y = y;
 }
 
 void
@@ -67,56 +75,46 @@ on_button_release (GtkWidget *widget, GdkEventButton *event)
 	if (!button_pressed)
 		return;
 
-	gtk_widget_get_allocation(widget, &allocation);
-
 	button_pressed = false;
-	if (event->type == GDK_BUTTON_RELEASE) {
-		if (!button_dragged) {
-			// We have a click, not a drag:
-			if (autoscroll_is_on()) {
-				autoscroll_stop();
-			}
-			else if (!click_halted_autoscroll) {
-				// Only recenter the viewport if this is the kind
-				// of button press that did not halt the autoscroll:
-				viewport_center_at(EVENT_X, EVENT_Y);
-			}
+
+	// We have just released a drag; kickoff the autoscroller:
+	if (button_dragged) {
+		event_get_xy;
+		autoscroll_may_start(x, y);
+		if (autoscroll_is_on())
 			gtk_widget_queue_draw(widget);
-		}
-		else {
-			// We have just released a drag; kickoff the autoscroller:
-			autoscroll_may_start(EVENT_X, EVENT_Y);
-			if (autoscroll_is_on()) {
-				gtk_widget_queue_draw(widget);
-			}
-		}
+
+		return;
 	}
+
+	// We have a click, not a drag:
+	if (!click_halted_autoscroll) {
+		// Only recenter the viewport if this is the kind
+		// of button press that did not halt the autoscroll:
+		event_get_xy;
+		viewport_center_at(x, y);
+	}
+
+	gtk_widget_queue_draw(widget);
 }
 
 void
 on_mouse_scroll (GtkWidget* widget, GdkEventScroll *event)
 {
-	int click_x;
-	int click_y;
-
-	gtk_widget_get_allocation(widget, &allocation);
-
 	switch (event->direction)
 	{
-	case GDK_SCROLL_UP:
-		click_x = EVENT_X;
-		click_y = EVENT_Y;
-		viewport_zoom_in(click_x, click_y);
+	case GDK_SCROLL_UP: {
+		event_get_xy;
+		viewport_zoom_in(x, y);
 		gtk_widget_queue_draw(widget);
 		break;
-
-	case GDK_SCROLL_DOWN:
-		click_x = EVENT_X;
-		click_y = EVENT_Y;
-		viewport_zoom_out(click_x, click_y);
+	}
+	case GDK_SCROLL_DOWN: {
+		event_get_xy;
+		viewport_zoom_out(x, y);
 		gtk_widget_queue_draw(widget);
 		break;
-
+	}
 	default:
 		break;
 	}
