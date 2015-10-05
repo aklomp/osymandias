f063ee8669abfc8c9644407ece958db8b0ded509
Alfred Klomp <git@alfredklomp.com>
2015-10-05 15:08:27 +0200

    world: zoom: simplify

--- world.c
+++ world.c
@@ -7,7 +7,7 @@
 // Current zoomlevel:
 static int world_zoom = ZOOM_MIN;
 
-// Current world size in pixels:
+// Current world size in tiles:
 static unsigned int world_size = ZOOM_SIZE(ZOOM_MIN);
 
 // NB: the current world contracts and expands based on zoom level.
@@ -23,14 +23,14 @@ world_get_size (void)
 	return world_size;
 }
 
-unsigned int
-world_get_size_at (const int zoomlevel)
+static unsigned int
+size_at (const int zoomlevel)
 {
 	// Zoom level 0 = 1 tile
 	// Zoom level 1 = 2 tiles
-	// Zoom level 2 = 3 tiles
+	// Zoom level 2 = 4 tiles
 	// Zoom level n = 2^n tiles
-	return ((unsigned int)1) << zoomlevel;
+	return 1U << zoomlevel;
 }
 
 int
@@ -39,29 +39,28 @@ world_get_zoom (void)
 	return world_zoom;
 }
 
-void
-world_zoom_to (const int zoomlevel)
+static bool
+zoom_to (const int zoomlevel)
 {
+	if (zoomlevel < ZOOM_MIN)
+		return false;
+
+	if (zoomlevel > ZOOM_MAX)
+		return false;
+
 	world_zoom = zoomlevel;
-	world_size = world_get_size_at(world_zoom);
+	world_size = size_at(world_zoom);
+	return true;
 }
 
 bool
 world_zoom_in (void)
 {
-	if (world_zoom == ZOOM_MAX) {
-		return false;
-	}
-	world_zoom_to(world_zoom + 1);
-	return true;
+	return zoom_to(world_zoom + 1);
 }
 
 bool
 world_zoom_out (void)
 {
-	if (world_zoom == ZOOM_MIN) {
-		return false;
-	}
-	world_zoom_to(world_zoom - 1);
-	return true;
+	return zoom_to(world_zoom - 1);
 }
--- world.h
+++ world.h
@@ -6,6 +6,5 @@ unsigned int world_get_size_at (const int zoomlevel);
 int world_get_zoom (void);
 bool world_zoom_in (void);
 bool world_zoom_out (void);
-void world_zoom_to (const int zoomlevel);
 
 #endif	/* WORLD_H */
