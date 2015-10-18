b5a48eed987119b2d6bfb811e91f12f52d6039d5
Alfred Klomp <git@alfredklomp.com>
2015-10-18 19:51:44 +0200

    Tiledrawer: use 'struct vector' for tidier access

--- tiledrawer.c
+++ tiledrawer.c
@@ -15,6 +15,13 @@ struct texture {
 	float offset_y;
 };
 
+struct vector {
+	float x;
+	float y;
+	float z;
+	float w;
+} __attribute__((packed));
+
 static void
 cutout_texture (float orig_x, float orig_y, float wd, float ht, const struct quadtree_req *req, struct texture *t)
 {
@@ -32,7 +39,7 @@ cutout_texture (float orig_x, float orig_y, float wd, float ht, const struct qua
 }
 
 static void
-draw_tile_planar (GLuint texture_id, const struct texture *t, float p[4][4])
+draw_tile_planar (GLuint texture_id, const struct texture *t, const struct vector p[4])
 {
 	GLdouble txoffs = (GLdouble)ldexpf(t->offset_x, -8);
 	GLdouble tyoffs = (GLdouble)ldexpf(t->offset_y, -8);
@@ -44,15 +51,15 @@ draw_tile_planar (GLuint texture_id, const struct texture *t, float p[4][4])
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	glBegin(GL_QUADS);
-		glTexCoord2f(txoffs,       tyoffs);       glVertex2f(p[0][0], p[0][1]);
-		glTexCoord2f(txoffs + twd, tyoffs);       glVertex2f(p[1][0], p[1][1]);
-		glTexCoord2f(txoffs + twd, tyoffs + tht); glVertex2f(p[2][0], p[2][1]);
-		glTexCoord2f(txoffs,       tyoffs + tht); glVertex2f(p[3][0], p[3][1]);
+		glTexCoord2f(txoffs,       tyoffs);       glVertex2f(p[0].x, p[0].y);
+		glTexCoord2f(txoffs + twd, tyoffs);       glVertex2f(p[1].x, p[1].y);
+		glTexCoord2f(txoffs + twd, tyoffs + tht); glVertex2f(p[2].x, p[2].y);
+		glTexCoord2f(txoffs,       tyoffs + tht); glVertex2f(p[3].x, p[3].y);
 	glEnd();
 }
 
 static void
-draw_tile_spherical (GLuint texture_id, const struct texture *t, float p[4][4])
+draw_tile_spherical (GLuint texture_id, const struct texture *t, const struct vector p[4])
 {
 	GLdouble txoffs = (GLdouble)ldexpf(t->offset_x, -8);
 	GLdouble tyoffs = (GLdouble)ldexpf(t->offset_y, -8);
@@ -64,15 +71,15 @@ draw_tile_spherical (GLuint texture_id, const struct texture *t, float p[4][4])
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	glBegin(GL_QUADS);
-		glNormal3f(p[0][0], p[0][1], p[0][2]); glTexCoord2f(txoffs,       tyoffs);       glVertex3f(p[0][0], p[0][1], p[0][2]);
-		glNormal3f(p[1][0], p[1][1], p[1][2]); glTexCoord2f(txoffs + twd, tyoffs);       glVertex3f(p[1][0], p[1][1], p[1][2]);
-		glNormal3f(p[2][0], p[2][1], p[2][2]); glTexCoord2f(txoffs + twd, tyoffs + tht); glVertex3f(p[2][0], p[2][1], p[2][2]);
-		glNormal3f(p[3][0], p[3][1], p[3][2]); glTexCoord2f(txoffs,       tyoffs + tht); glVertex3f(p[3][0], p[3][1], p[3][2]);
+		glNormal3f(p[0].x, p[0].y, p[0].z); glTexCoord2f(txoffs,       tyoffs);       glVertex3f(p[0].x, p[0].y, p[0].z);
+		glNormal3f(p[1].x, p[1].y, p[1].z); glTexCoord2f(txoffs + twd, tyoffs);       glVertex3f(p[1].x, p[1].y, p[1].z);
+		glNormal3f(p[2].x, p[2].y, p[2].z); glTexCoord2f(txoffs + twd, tyoffs + tht); glVertex3f(p[2].x, p[2].y, p[2].z);
+		glNormal3f(p[3].x, p[3].y, p[3].z); glTexCoord2f(txoffs,       tyoffs + tht); glVertex3f(p[3].x, p[3].y, p[3].z);
 	glEnd();
 }
 
 void
-tiledrawer (float tile_x, float tile_y, float tile_wd, float tile_ht, GLuint texture_id, const struct quadtree_req *req, float p[4][4])
+tiledrawer (float tile_x, float tile_y, float tile_wd, float tile_ht, GLuint texture_id, const struct quadtree_req *req, struct vector *p)
 {
 	struct texture t;
 
@@ -82,10 +89,14 @@ tiledrawer (float tile_x, float tile_y, float tile_wd, float tile_ht, GLuint tex
 	cutout_texture(tile_x, tile_y, tile_wd, tile_ht, req, &t);
 
 	// With this cutout information in hand, draw the actual tile:
-	if (viewport_mode_get() == VIEWPORT_MODE_PLANAR) {
+	switch (viewport_mode_get())
+	{
+	case VIEWPORT_MODE_PLANAR:
 		draw_tile_planar(texture_id, &t, p);
-	}
-	if (viewport_mode_get() == VIEWPORT_MODE_SPHERICAL) {
+		break;
+
+	case VIEWPORT_MODE_SPHERICAL:
 		draw_tile_spherical(texture_id, &t, p);
+		break;
 	}
 }
