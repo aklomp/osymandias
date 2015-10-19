1f1282bc393b8761ff648234da64cb4309939a84
Alfred Klomp <git@alfredklomp.com>
2015-10-19 23:24:10 +0200

    coord3h.h: split model building into rot, scale, trans

--- coord3d.h
+++ coord3d.h
@@ -9,8 +9,7 @@ static inline double
 world_y_to_lat (double y, double world_size)
 {
 	// ybottom is ymin is -pi, ytop is ymax is pi:
-	double lat = (y / world_size - 0.5) * 2.0 * M_PI;
-	return atan(sinh(lat));
+	return atan(sinh((y / world_size - 0.5) * 2.0 * M_PI));
 }
 
 static inline double
@@ -24,9 +23,9 @@ latlon_to_xyz (const double lat, const double lon, const double world_size, cons
 {
 	const double world_radius = world_size / M_PI;
 
-	*x = cos(lat) * sin(lon - viewlon) * world_radius;
-	*z = cos(lat) * cos(lon - viewlon) * world_radius;
-	*y = sin(lat) * world_radius;
+	*x = cos(lat) * sin(lon - viewlon);
+	*z = cos(lat) * cos(lon - viewlon);
+	*y = sin(lat);
 
 	// Rotate the points over lat radians via x axis:
 	double yorig = *y;
@@ -35,7 +34,12 @@ latlon_to_xyz (const double lat, const double lon, const double world_size, cons
 	*y = yorig * cos_viewlat - zorig * sin_viewlat;
 	*z = yorig * sin_viewlat + zorig * cos_viewlat;
 
-	// Push the world "back" from the camera
+	// Scale the world to its full size:
+	*x *= world_radius;
+	*y *= world_radius;
+	*z *= world_radius;
+
+	// Translate the world "back" from the camera,
 	// so that the cursor (centerpoint) is at (0,0,0):
 	*z -= world_radius;
 }
