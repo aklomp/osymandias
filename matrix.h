21c19cb68ac8e830c2bdc5048438f598cab72c75
Alfred Klomp <git@alfredklomp.com>
2015-10-25 20:58:08 +0100

    matrix.h: mat_vec_multiply(): mark arguments as const

--- matrix.h
+++ matrix.h
@@ -4,5 +4,5 @@ void mat_rotate (float *matrix, float x, float y, float z, float angle);
 void mat_scale (float *matrix, float x, float y, float z);
 void mat_ortho (float *matrix, float left, float right, float bottom, float top, float near, float far);
 void mat_frustum (float *matrix, float angle_of_view, float aspect_ratio, float z_near, float z_far);
-void mat_vec_multiply (float *vector, float *m, float *v);
+void mat_vec_multiply (float *vector, const float *m, const float *v);
 void mat_multiply (float *matrix, const float *a, const float *b);
