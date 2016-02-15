4c5bdc50b6d0eb168bff7f0d7b228b1879f1c5bc
Alfred Klomp <git@alfredklomp.com>
2016-02-15 23:01:54 +0100

    Quadtree: small cleanups

--- quadtree.c
+++ quadtree.c
@@ -37,14 +37,20 @@ struct quadtree {
 	int stamp;
 };
 
+// A composable loop header that loops over all valid quads:
+#define FOREACH_QUAD(node)			\
+	for (int x = 0; x < 2; x++)		\
+		for (int y = 0; y < 2; y++)	\
+			if ((node)->q[x][y])
+
 static struct node *
 node_create (struct quadtree *t, struct node *parent, int x, int y)
 {
 	struct node *n;
 
-	if ((n = malloc(sizeof(*n))) == NULL) {
+	if ((n = malloc(sizeof(*n))) == NULL)
 		return NULL;
-	}
+
 	n->x = x;
 	n->y = y;
 	n->data = NULL;
@@ -72,15 +78,13 @@ node_destroy_recursive (struct quadtree *t, struct node *n)
 	// Deletes this node and all of its children.
 	// Updating the parent node is the responsibility of the caller.
 
-	if (n == NULL) return;
+	if (n == NULL)
+		return;
 
 	node_data_destroy(t, n);
 
-	// Unroll loop, save a stack variable:
-	if (n->q[0][0]) node_destroy_recursive(t, n->q[0][0]);
-	if (n->q[0][1]) node_destroy_recursive(t, n->q[0][1]);
-	if (n->q[1][0]) node_destroy_recursive(t, n->q[1][0]);
-	if (n->q[1][1]) node_destroy_recursive(t, n->q[1][1]);
+	FOREACH_QUAD(n)
+		node_destroy_recursive(t, n->q[x][y]);
 
 	free(n);
 
@@ -116,7 +120,8 @@ purge_node_if_obsolete (struct quadtree *t, struct node *n)
 static void
 node_destroy_self (struct quadtree *t, struct node *n)
 {
-	if (n == NULL) return;
+	if (n == NULL)
+		return;
 
 	int qx = n->x & 1;
 	int qy = n->y & 1;
@@ -124,7 +129,8 @@ node_destroy_self (struct quadtree *t, struct node *n)
 
 	node_destroy_recursive(t, n);
 
-	if (parent) parent->q[qx][qy] = NULL;
+	if (parent)
+		parent->q[qx][qy] = NULL;
 }
 
 static void
@@ -135,10 +141,12 @@ purge_below (struct quadtree *t, struct node *n, int zoom, int prune_target)
 		node_destroy_self(t, n);
 		return;
 	}
-	if (n->q[0][0]) { purge_below(t, n->q[0][0], zoom, prune_target); if (t->num_data <= prune_target) return; }
-	if (n->q[0][1]) { purge_below(t, n->q[0][1], zoom, prune_target); if (t->num_data <= prune_target) return; }
-	if (n->q[1][0]) { purge_below(t, n->q[1][0], zoom, prune_target); if (t->num_data <= prune_target) return; }
-	if (n->q[1][1]) { purge_below(t, n->q[1][1], zoom, prune_target); }
+
+	FOREACH_QUAD(n) {
+		purge_below(t, n->q[x][y], zoom, prune_target);
+		if (t->num_data <= prune_target)
+			return;
+	}
 }
 
 static void
@@ -146,17 +154,17 @@ node_update_viewzoom (struct quadtree *t, struct node *n)
 {
 	// If tree and node have the same "stamp", we already did this
 	// expensive calculation for this round:
-	if (n->stamp == t->stamp) {
+	if (n->stamp == t->stamp)
 		return;
-	}
+
 	// Calculate zoom levels for the four quads of this node:
 	int zoomdiff = t->world_zoom - n->zoom;
 
 	// Get max zoom with this special function, that not only takes into
 	// account the corner tiles, but also the tile directly under the
 	// cursor:
-	n->viewzoom_max = tile2d_get_max_zoom
-	(
+	n->viewzoom_max = tile2d_get_max_zoom (
+
 		// Coordinate of this tile at current world zoom:
 		n->x << zoomdiff,
 		n->y << zoomdiff,
@@ -167,9 +175,10 @@ node_update_viewzoom (struct quadtree *t, struct node *n)
 		// Current view data:
 		t->center, t->world_zoom
 	);
+
 	// Get min zoom by getting the corner zooms of the tile:
-	vec4i zooms = tile2d_get_corner_zooms
-	(
+	vec4i zooms = tile2d_get_corner_zooms (
+
 		// Coordinate of this tile at current world zoom:
 		n->x << zoomdiff,
 		n->y << zoomdiff,
@@ -197,10 +206,12 @@ purge_below_visibility (struct quadtree *t, struct node *n, int prune_target)
 		node_destroy_self(t, n);
 		return;
 	}
-	if (n->q[0][0]) { purge_below_visibility(t, n->q[0][0], prune_target); if (t->num_data <= prune_target) return; }
-	if (n->q[0][1]) { purge_below_visibility(t, n->q[0][1], prune_target); if (t->num_data <= prune_target) return; }
-	if (n->q[1][0]) { purge_below_visibility(t, n->q[1][0], prune_target); if (t->num_data <= prune_target) return; }
-	if (n->q[1][1]) { purge_below_visibility(t, n->q[1][1], prune_target); }
+
+	FOREACH_QUAD(n) {
+		purge_below_visibility(t, n->q[x][y], prune_target);
+		if (t->num_data <= prune_target)
+			return;
+	}
 }
 
 static void
@@ -208,7 +219,8 @@ quadtree_prune (struct quadtree *t, struct node *n, int prune_target)
 {
 	// Purge all tiles below current zoom level:
 	purge_below(t, t->root, t->world_zoom, prune_target);
-	if (t->num_data <= prune_target) return;
+	if (t->num_data <= prune_target)
+		return;
 
 	// Calculate visible zoom level (a function of distance) for
 	// all tiles, delete those that are "too high-res":
@@ -221,9 +233,9 @@ node_find_parent_for (const struct node *a, int zoom, int x, int y)
 	struct node *n = (struct node *)a;
 
 	// The parent is always at same or lower zoom level than given:
-	while (n->zoom > zoom) {
+	while (n->zoom > zoom)
 		n = n->parent;
-	}
+
 	x >>= zoom - n->zoom;
 	y >>= zoom - n->zoom;
 	int nx = n->x;
@@ -244,9 +256,9 @@ quadtree_create (int capacity,
 {
 	struct quadtree *t;
 
-	if ((t = malloc(sizeof(*t))) == NULL) {
+	if ((t = malloc(sizeof(*t))) == NULL)
 		return NULL;
-	}
+
 	t->num_data = 0;
 	t->num_nodes = 0;
 	t->stamp = 0;
@@ -275,24 +287,22 @@ static bool
 _quadtree_request (struct quadtree *t, struct quadtree_req *req, struct node *n)
 {
 	// Node is too low in the hierarchy? Find common parent, start there:
-	if (req->zoom < n->zoom || (req->zoom == n->zoom && (req->x != n->x || req->y != n->y))) {
+	if (req->zoom < n->zoom || (req->zoom == n->zoom && (req->x != n->x || req->y != n->y)))
 		return _quadtree_request(t, req, node_find_parent_for(n, req->zoom, req->x, req->y));
-	}
+
 	// Not at the right node yet? Go deeper:
-	else if (req->zoom > n->zoom)
-	{
+	if (req->zoom > n->zoom) {
 		int qx = (req->x >> (req->zoom - n->zoom - 1)) & 1;
 		int qy = (req->y >> (req->zoom - n->zoom - 1)) & 1;
 		struct node *q = n->q[qx][qy];
 
 		// Successfully got the data from a lower level:
-		if (q != NULL && _quadtree_request(t, req, q)) {
+		if (q != NULL && _quadtree_request(t, req, q))
 			return true;
-		}
 	}
 	// Otherwise, use this node's data if present:
-	if (n->data == NULL || n->zoom < req->zoom)
-	{
+	if (n->data == NULL || n->zoom < req->zoom) {
+
 		// Last effort: procure data:
 		if (t->tile_procure && (req->found_data = t->tile_procure(req)) != NULL) {
 			if (quadtree_data_insert(t, req, req->found_data)) {
@@ -305,9 +315,8 @@ _quadtree_request (struct quadtree *t, struct quadtree_req *req, struct node *n)
 			return false;
 		}
 		// If this tile has no data either, give up:
-		if (n->data == NULL) {
+		if (n->data == NULL)
 			return false;
-		}
 	}
 	req->found_x = n->x;
 	req->found_y = n->y;
@@ -326,18 +335,18 @@ static inline void
 node_data_update (struct quadtree *t, struct node *n, void *data)
 {
 	// Delete any existing data at this location:
-	if (n->data != NULL) {
+	if (n->data != NULL)
 		node_data_destroy(t, n);
-	}
-	if ((n->data = data) != NULL)
-	{
+
+	if ((n->data = data) != NULL) {
+
 		// Maybe the parent is now made obsolete by this new data:
 		// TODO: we don't always want to delete the parent straight away.
 		purge_node_if_obsolete(t, n->parent);
 
 		// Went over capacity? Purge:
-		if (++t->num_data > t->capacity)
-		{
+		if (++t->num_data > t->capacity) {
+
 			// Amount of filled nodes to purge down to:
 			quadtree_prune(t, t->root, (t->capacity * 3) / 4);
 		}
@@ -350,8 +359,7 @@ quadtree_data_insert (struct quadtree *t, struct quadtree_req *req, void *data)
 	struct node *n = t->root;
 
 	// Reach node from root, create nonexistent nodes:
-	while (n->zoom < req->zoom)
-	{
+	while (n->zoom < req->zoom) {
 		int qx = (req->x >> (req->zoom - n->zoom - 1)) & 1;
 		int qy = (req->y >> (req->zoom - n->zoom - 1)) & 1;
 
