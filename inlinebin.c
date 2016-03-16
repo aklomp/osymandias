4c857778f04aa8324157c7947610623a7f866741
Alfred Klomp <git@alfredklomp.com>
2016-03-16 21:28:11 +0100

    inlinebin: use "X macro" trick to avoid duplication

--- inlinebin.c
+++ inlinebin.c
@@ -5,7 +5,7 @@
 #include "inlinebin.h"
 
 #define DATA_DEF(name)							\
-	extern const uint8_t _binary_## name ##_start[];			\
+	extern const uint8_t _binary_## name ##_start[];		\
 	extern const uint8_t _binary_## name ##_end[];
 
 #define DATA_ELEM(name)							\
@@ -13,15 +13,20 @@
 	, .len = _binary_## name ##_end - _binary_## name ##_start	\
 	} ,
 
+#define X_MAP					\
+	X (shaders_cursor_vertex_glsl)		\
+	X (shaders_cursor_fragment_glsl)	\
+	X (shaders_bkgd_vertex_glsl)		\
+	X (shaders_bkgd_fragment_glsl)		\
+	X (shaders_solid_vertex_glsl)		\
+	X (shaders_solid_fragment_glsl)		\
+	X (shaders_frustum_vertex_glsl)		\
+	X (shaders_frustum_fragment_glsl)
+
 // Define variables pointing to inline data:
-DATA_DEF (shaders_cursor_vertex_glsl)
-DATA_DEF (shaders_cursor_fragment_glsl)
-DATA_DEF (shaders_bkgd_vertex_glsl)
-DATA_DEF (shaders_bkgd_fragment_glsl)
-DATA_DEF (shaders_solid_vertex_glsl)
-DATA_DEF (shaders_solid_fragment_glsl)
-DATA_DEF (shaders_frustum_vertex_glsl)
-DATA_DEF (shaders_frustum_fragment_glsl)
+#define X(x) DATA_DEF(x)
+X_MAP
+#undef X
 
 void
 inlinebin_get (enum inlinebin member, const void **buf, size_t *len)
@@ -32,17 +37,16 @@ inlinebin_get (enum inlinebin member, const void **buf, size_t *len)
 		size_t		 len;
 	}
 	elems[] = {
+
+		// First element is empty sentinel:
 		{ .buf = NULL
 		, .len = 0
 		} ,
-		DATA_ELEM (shaders_cursor_vertex_glsl)
-		DATA_ELEM (shaders_cursor_fragment_glsl)
-		DATA_ELEM (shaders_bkgd_vertex_glsl)
-		DATA_ELEM (shaders_bkgd_fragment_glsl)
-		DATA_ELEM (shaders_solid_vertex_glsl)
-		DATA_ELEM (shaders_solid_fragment_glsl)
-		DATA_ELEM (shaders_frustum_vertex_glsl)
-		DATA_ELEM (shaders_frustum_fragment_glsl)
+
+		// Populate remaining elements:
+		#define X(x) DATA_ELEM(x)
+		X_MAP
+		#undef X
 	};
 
 	// Return requested element to caller:
