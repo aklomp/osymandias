35f1c21920b697250323301e5303918a9edb6744
Alfred Klomp <git@alfredklomp.com>
2019-04-08 21:13:17 +0200

    layers: basemap: remove planar basemap
    The planar basemap was broken for a while.

--- inlinebin.c
+++ inlinebin.c
@@ -11,27 +11,26 @@
 		     - _binary_## name ##_start,			\
 	},
 
-#define X_MAP											\
-	X (SHADER_BASEMAP_SPHERICAL_VERTEX,	shaders_basemap_spherical_vertex_glsl)		\
-	X (SHADER_BASEMAP_SPHERICAL_FRAGMENT,	shaders_basemap_spherical_fragment_glsl)	\
-	X (SHADER_BKGD_VERTEX,			shaders_bkgd_vertex_glsl)			\
-	X (SHADER_BKGD_FRAGMENT,		shaders_bkgd_fragment_glsl)			\
-	X (SHADER_FRUSTUM_VERTEX,		shaders_frustum_vertex_glsl)			\
-	X (SHADER_FRUSTUM_FRAGMENT,		shaders_frustum_fragment_glsl)			\
-	X (SHADER_PLANAR_VERTEX,		shaders_planar_vertex_glsl)			\
-	X (SHADER_PLANAR_FRAGMENT,		shaders_planar_fragment_glsl)			\
-	X (SHADER_SOLID_VERTEX,			shaders_solid_vertex_glsl)			\
-	X (SHADER_SOLID_FRAGMENT,		shaders_solid_fragment_glsl)			\
-	X (SHADER_SPHERICAL_VERTEX,		shaders_spherical_vertex_glsl)			\
-	X (SHADER_SPHERICAL_FRAGMENT,		shaders_spherical_fragment_glsl)		\
-	X (SHADER_TILE2D_VERTEX,		shaders_tile2d_vertex_glsl)			\
-	X (SHADER_TILE2D_FRAGMENT,		shaders_tile2d_fragment_glsl)			\
-	X (SHADER_TILEPICKER_VERTEX,		shaders_tilepicker_vertex_glsl)			\
-	X (SHADER_TILEPICKER_FRAGMENT,		shaders_tilepicker_fragment_glsl)		\
-	X (TEXTURE_BACKGROUND,			textures_background_png)			\
-	X (TEXTURE_BASEMAP,			textures_basemap_png)				\
-	X (TEXTURE_CURSOR,			textures_cursor_png)				\
-	X (TEXTURE_COPYRIGHT,			textures_copyright_png)				\
+#define X_MAP										\
+	X (SHADER_BASEMAP_VERTEX,	shaders_basemap_spherical_vertex_glsl)		\
+	X (SHADER_BASEMAP_FRAGMENT,	shaders_basemap_spherical_fragment_glsl)	\
+	X (SHADER_BKGD_VERTEX,		shaders_bkgd_vertex_glsl)			\
+	X (SHADER_BKGD_FRAGMENT,	shaders_bkgd_fragment_glsl)			\
+	X (SHADER_FRUSTUM_VERTEX,	shaders_frustum_vertex_glsl)			\
+	X (SHADER_FRUSTUM_FRAGMENT,	shaders_frustum_fragment_glsl)			\
+	X (SHADER_PLANAR_VERTEX,	shaders_planar_vertex_glsl)			\
+	X (SHADER_PLANAR_FRAGMENT,	shaders_planar_fragment_glsl)			\
+	X (SHADER_SOLID_VERTEX,		shaders_solid_vertex_glsl)			\
+	X (SHADER_SOLID_FRAGMENT,	shaders_solid_fragment_glsl)			\
+	X (SHADER_SPHERICAL_VERTEX,	shaders_spherical_vertex_glsl)			\
+	X (SHADER_SPHERICAL_FRAGMENT,	shaders_spherical_fragment_glsl)		\
+	X (SHADER_TILE2D_VERTEX,	shaders_tile2d_vertex_glsl)			\
+	X (SHADER_TILE2D_FRAGMENT,	shaders_tile2d_fragment_glsl)			\
+	X (SHADER_TILEPICKER_VERTEX,	shaders_tilepicker_vertex_glsl)			\
+	X (SHADER_TILEPICKER_FRAGMENT,	shaders_tilepicker_fragment_glsl)		\
+	X (TEXTURE_BACKGROUND,		textures_background_png)			\
+	X (TEXTURE_CURSOR,		textures_cursor_png)				\
+	X (TEXTURE_COPYRIGHT,		textures_copyright_png)				\
 
 // Define variables pointing to inline data:
 #define X(id, name) DATA_DEF(name)
