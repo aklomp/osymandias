c9e71e076ddeedc4770ffba6e20554a5aaef7d80
Alfred Klomp <git@alfredklomp.com>
2019-05-04 23:42:02 +0200

    tiledrawer: pass cached tile 3D coordinates to shader

--- tiledrawer.c
+++ tiledrawer.c
@@ -3,6 +3,32 @@
 #include "programs.h"
 #include "programs/spherical.h"
 
+// Array of indices. If we have a quad defined by these corners:
+//
+//   3--2
+//   |  |
+//   0--1
+//
+// we define two counterclockwise triangles, 0-2-3 and 0-1-2:
+static const uint8_t vertex_index[] = {
+	0, 2, 3,
+	0, 1, 2,
+};
+
+// Per-vertex data sent to OpenGL.
+struct vertex {
+	struct vertex_texture {
+		float u;
+		float v;
+	} __attribute__((packed)) texture;
+	struct cache_data_coords coords;
+} __attribute__((packed));
+
+static struct {
+	uint32_t vao;
+	uint32_t vbo;
+} state;
+
 void
 tiledrawer (const struct tiledrawer *td)
 {
@@ -10,26 +36,12 @@ tiledrawer (const struct tiledrawer *td)
 		return;
 
 	// Texture coordinates:
-	struct glutil_vertex_uv texuv[4] = {
-		{ .u = 0.0f, 0.0f },
-		{ .u = 1.0f, 0.0f },
-		{ .u = 1.0f, 1.0f },
-		{ .u = 0.0f, 1.0f },
+	struct vertex vertex[] = {
+		{ .texture.u = 0.0f, .texture.v = 1.0f, .coords = td->data->coords.sw },
+		{ .texture.u = 1.0f, .texture.v = 1.0f, .coords = td->data->coords.se },
+		{ .texture.u = 1.0f, .texture.v = 0.0f, .coords = td->data->coords.ne },
+		{ .texture.u = 0.0f, .texture.v = 0.0f, .coords = td->data->coords.nw },
 	};
-
-	// Vertex coordinates, translating tile coordinate order to OpenGL
-	// coordinate order:
-	//
-	//  OpenGL   Tile
-	//
-	//   3--2    0,0----wd,0
-	//   |  |     |      |
-	//   0--1    0,ht--wd,ht
-	//
-	texuv[0].x = texuv[3].x = td->tile->x;
-	texuv[1].x = texuv[2].x = td->tile->x + 1;
-	texuv[0].y = texuv[1].y = td->tile->y;
-	texuv[3].y = texuv[2].y = td->tile->y + 1;
 
 	// Set tile zoom level:
 	program_spherical_set_tile(td->tile);
@@ -41,29 +53,50 @@ tiledrawer (const struct tiledrawer *td)
 	glBindTexture(GL_TEXTURE_2D, td->data->u32);
 
 	// Copy vertices to buffer:
-	glBufferData(GL_ARRAY_BUFFER, sizeof (texuv), texuv, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, state.vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof (vertex), vertex, GL_DYNAMIC_DRAW);
 
-	// Draw all triangles in the buffer:
-	glutil_draw_quad();
+	// Draw two triangles which together form one quad:
+	glDrawElements(GL_TRIANGLES, sizeof (vertex_index), GL_UNSIGNED_BYTE, vertex_index);
+}
+
+void
+tiledrawer_init (void)
+{
+	const int32_t loc_texture = program_spherical_loc(LOC_SPHERICAL_TEXTURE);
+	const int32_t loc_vertex  = program_spherical_loc(LOC_SPHERICAL_VERTEX);
+
+	glGenVertexArrays(1, &state.vao);
+	glGenBuffers(1, &state.vbo);
+	glBindVertexArray(state.vao);
+	glBindBuffer(GL_ARRAY_BUFFER, state.vbo);
+
+	// Add pointer to texture coordinates:
+	glEnableVertexAttribArray(loc_texture);
+	glVertexAttribPointer(loc_texture, 2, GL_FLOAT, GL_FALSE,
+			sizeof (struct vertex),
+			(void *) offsetof(struct vertex, texture));
+
+	// Add pointer to 3D vertex coordinates:
+	glEnableVertexAttribArray(loc_vertex);
+	glVertexAttribPointer(loc_vertex, 3, GL_FLOAT, GL_FALSE,
+			sizeof (struct vertex),
+			(void *) offsetof(struct vertex, coords));
 }
 
 void
 tiledrawer_start (const struct camera *cam, const struct globe *globe)
 {
-	static GLuint vao;
 	static bool init = false;
 
 	// Lazy init:
 	if (init == false) {
-		glGenVertexArrays(1, &vao);
+		tiledrawer_init();
 		init = true;
 	}
 
-	glBindVertexArray(vao);
-
-	glutil_vertex_uv_link(
-		program_spherical_loc(LOC_SPHERICAL_VERTEX),
-		program_spherical_loc(LOC_SPHERICAL_TEXTURE));
+	glBindVertexArray(state.vao);
+	glBindBuffer(GL_ARRAY_BUFFER, state.vbo);
 
 	program_spherical_use(&((struct program_spherical) {
 		.mat_viewproj = cam->matrix.viewproj,
