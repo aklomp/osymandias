3bb22d76f98871e710ef46010b11a8603273b573
Alfred Klomp <git@alfredklomp.com>
2019-05-12 18:58:53 +0200

    tilepicker: slightly wider view angle to include tiles at sides
    Slightly widen the tilepicker's view angle so that it includes more
    tiles around the edges of the window.

--- tilepicker.c
+++ tilepicker.c
@@ -90,7 +90,7 @@ render (const struct viewport *vp, const struct camera *cam)
 	program_tilepicker_use(&(struct program_tilepicker) {
 		.mat_model_inv = globe_get()->invert.model,
 		.mat_view_inv  = cam->matrix.inverse.view,
-		.vp_angle      = cam->view_angle,
+		.vp_angle      = cam->view_angle * (IMGSIZE + 2) / IMGSIZE,
 		.vp_height     = vp->height,
 		.vp_width      = vp->width,
 	});
