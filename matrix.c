7c1907bc09f3ed65339ce69a9ee58351e51fb3f7
Alfred Klomp <git@alfredklomp.com>
2019-06-01 11:17:03 +0200

    matrix: remove unused matrix_identity()

--- matrix.c
+++ matrix.c
@@ -3,27 +3,6 @@
 #include "matrix.h"
 
 void
-mat_identity (float *matrix)
-{
-	matrix[0] = 1;
-	matrix[1] = 0;
-	matrix[2] = 0;
-	matrix[3] = 0;
-	matrix[4] = 0;
-	matrix[5] = 1;
-	matrix[6] = 0;
-	matrix[7] = 0;
-	matrix[8] = 0;
-	matrix[9] = 0;
-	matrix[10] = 1;
-	matrix[11] = 0;
-	matrix[12] = 0;
-	matrix[13] = 0;
-	matrix[14] = 0;
-	matrix[15] = 1;
-}
-
-void
 mat_translate (float *matrix, float dx, float dy, float dz)
 {
 	matrix[0] = 1;
--- matrix.h
+++ matrix.h
@@ -1,11 +1,10 @@
 #pragma once
 
-void mat_identity (float *matrix);
-void mat_translate (float *matrix, float dx, float dy, float dz);
-void mat_rotate (float *matrix, float x, float y, float z, float angle);
-void mat_scale (float *matrix, float x, float y, float z);
-void mat_ortho (float *matrix, float left, float right, float bottom, float top, float near, float far);
-void mat_frustum (float *matrix, float angle_of_view, float aspect_ratio, float z_near, float z_far);
-void mat_vec_multiply (float *vector, const float *m, const float *v);
-void mat_multiply (float *matrix, const float *a, const float *b);
-void mat_invert (float *matrix, const float *m);
+extern void mat_translate (float *matrix, float dx, float dy, float dz);
+extern void mat_rotate (float *matrix, float x, float y, float z, float angle);
+extern void mat_scale (float *matrix, float x, float y, float z);
+extern void mat_ortho (float *matrix, float left, float right, float bottom, float top, float near, float far);
+extern void mat_frustum (float *matrix, float angle_of_view, float aspect_ratio, float z_near, float z_far);
+extern void mat_vec_multiply (float *vector, const float *m, const float *v);
+extern void mat_multiply (float *matrix, const float *a, const float *b);
+extern void mat_invert (float *matrix, const float *m);
