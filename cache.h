ec14535c53312e5e6b1d83316c0eebffbca8e718
Alfred Klomp <git@alfredklomp.com>
2019-10-08 22:09:08 +0200

    cache: support cache entries of variable size
    Support cache entries with variable size. This means that the cache can
    contain many different items, from single pointers to very large
    structures. This obviates the need for users to construct their own
    schemes on top of the cache to store large or complex entries. It also
    keeps the size of the "core" cache_node structure down.
    
    Internally, the entries are stored inside a linear array which is
    indexed by the cache nodes.
    
    This commit does not compile on its own.

--- cache.h
+++ cache.h
@@ -6,47 +6,26 @@
 
 struct cache;
 
-// Structure to hold coordinates in 3D space for the tile.
-struct cache_data_coords {
-	float x;
-	float y;
-	float z;
-} __attribute__((packed));
-
-// Union of data types that can be stored in the cache.
-struct cache_data {
-	union {
-		void    *ptr;
-		uint32_t u32;
-	};
-
-	// 3D spherical coordinates of the tile. These are cached because they
-	// are calculated by the host in double precision (which is relatively
-	// expensive), and because they remain constant for the lifetime of the
-	// tile. It would be possible to calculate these coordinates in a
-	// shader, but some video cards do not have sufficient floating point
-	// accuracy to get acceptable results. For ease of rendering, the
-	// storage order is counterclockwise starting from bottom left.
-	struct {
-		struct cache_data_coords sw;	// Southwest
-		struct cache_data_coords se;	// Southeast
-		struct cache_data_coords ne;	// Northeast
-		struct cache_data_coords nw;	// Northwest
-	} __attribute__((packed)) coords;
-};
-
 struct cache_config {
 
-	// Total maximum number of active cache members.
+	// Total maximum number of active cache entries.
 	size_t capacity;
 
-	// Callback function to call when an element is purged.
-	void (* destroy) (struct cache_data *);
+	// Size of one cache entry in bytes.
+	size_t entrysize;
+
+	// Callback function to call when an entry is purged.
+	void (* destroy) (void *);
 };
 
 struct cache_node {
-	uint32_t x;
-	uint32_t y;
+	union {
+		struct {
+			uint32_t x;
+			uint32_t y;
+		} __attribute__((packed));
+		uint64_t key;
+	};
 	uint32_t zoom;
 };
 
@@ -65,11 +44,11 @@ static inline bool cache_node_up (struct cache_node *n)
 // Insert opaque data into the cache at a given level. If a node exists for the
 // location, it is reused. If the insertion would exceed the cache capacity,
 // the least active cache node is purged first to make space for the insertion.
-extern const struct cache_data *cache_insert (struct cache *cache, const struct cache_node *loc, struct cache_data *data);
+extern void *cache_insert (struct cache *cache, const struct cache_node *loc, void *data);
 
 // Retrieve data from the cache at a given level. The function returns data at
 // this zoom level or lower. The #out member describes the returned node.
-extern const struct cache_data *cache_search (struct cache *cache, const struct cache_node *in, struct cache_node *out);
+extern void *cache_search (struct cache *cache, const struct cache_node *in, struct cache_node *out);
 
 // Cache creation/destruction.
 extern void cache_destroy (struct cache *cache);
