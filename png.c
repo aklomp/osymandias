20abcb4698a311a65acdadcf14f3b21277ccd60e
Alfred Klomp <git@alfredklomp.com>
2022-07-17 01:39:53 +0200

    png: refactor, remove error goto's

--- png.c
+++ png.c
@@ -5,11 +5,17 @@
 
 #include "png.h"
 
-// Structure for memory read I/O.
-struct io {
-	const uint8_t *buf;
-	const uint8_t *cur;
-	size_t         len;
+// Local state structure.
+struct state {
+	png_structp pngp;
+	png_infop   infop;
+
+	// Structure for memory read I/O.
+	struct io {
+		const uint8_t *buf;
+		const uint8_t *cur;
+		size_t         len;
+	} io;
 };
 
 // Our own I/O functions:
@@ -57,13 +63,15 @@ is_png (const struct png_in *in)
 	return png_sig_cmp(in->buf, 0, (in->len > 8) ? 8 : in->len) == 0;
 }
 
-bool
-png_load (const struct png_in *in, struct png_out *out)
+static void
+deinit (struct state *state)
 {
-	png_structp png_ptr;
-	png_infop info_ptr;
-	bool err = false;
+	png_destroy_read_struct(&state->pngp, &state->infop, NULL);
+}
 
+static bool
+init (struct state *state, const struct png_in *in, struct png_out *out)
+{
 	// Reset the caller-supplied output pointer.
 	out->buf = NULL;
 
@@ -72,77 +80,75 @@ png_load (const struct png_in *in, struct png_out *out)
 		return false;
 
 	// Create the read structure.
-	png_ptr = png_create_read_struct(
+	state->pngp = png_create_read_struct(
 		PNG_LIBPNG_VER_STRING,
 		(void *) in,
 		error_fn,
 		warning_fn
 	);
 
-	if (png_ptr == NULL)
+	if (state->pngp == NULL)
 		return false;
 
 	// Create the info structure.
-	if ((info_ptr = png_create_info_struct(png_ptr)) == NULL) {
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+	if ((state->infop = png_create_info_struct(state->pngp)) == NULL) {
+		deinit(state);
 		return false;
 	}
 
 	// Set up the read I/O structure.
-	struct io io = {
+	state->io = (struct io) {
 		.len = in->len,
 		.buf = in->buf,
 		.cur = in->buf,
 	};
 
 	// Register a custom read function.
-	png_set_read_fn(png_ptr, &io, read_fn);
+	png_set_read_fn(state->pngp, &state->io, read_fn);
 
-	// Return here on errors.
-	if (setjmp(png_jmpbuf(png_ptr))) {
-		err = true;
-		goto exit;
-	}
+	return true;
+}
 
+static bool
+load (struct state *state, struct png_out *out)
+{
 	// Read the PNG up to the image data.
-	png_read_info(png_ptr, info_ptr);
+	png_read_info(state->pngp, state->infop);
 
 	// Transform the input to 8-bit RGB.
-	png_byte color_type = png_get_color_type (png_ptr, info_ptr);
-	png_byte bit_depth  = png_get_bit_depth  (png_ptr, info_ptr);
+	png_byte color_type = png_get_color_type (state->pngp, state->infop);
+	png_byte bit_depth  = png_get_bit_depth  (state->pngp, state->infop);
 
 	// Convert paletted images to RGB.
 	if (color_type == PNG_COLOR_TYPE_PALETTE)
-		png_set_palette_to_rgb(png_ptr);
+		png_set_palette_to_rgb(state->pngp);
 
 	// Upsample low-bit grayscale images to 8-bit.
 	if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
-		png_set_expand_gray_1_2_4_to_8(png_ptr);
+		png_set_expand_gray_1_2_4_to_8(state->pngp);
 
 	// Upsample low-bit images to 8-bit.
 	if (bit_depth < 8)
-		png_set_packing(png_ptr);
+		png_set_packing(state->pngp);
 
 	// Discard the alpha channel.
 	if (color_type & PNG_COLOR_MASK_ALPHA)
-		png_set_strip_alpha(png_ptr);
+		png_set_strip_alpha(state->pngp);
 
 	// Upsample grayscale to RGB.
 	if (color_type == PNG_COLOR_TYPE_GRAY
 	 || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
-		png_set_gray_to_rgb(png_ptr);
+		png_set_gray_to_rgb(state->pngp);
 
 	// Update the image info and populate the output structure.
-	png_read_update_info(png_ptr, info_ptr);
-	out->width    = png_get_image_width  (png_ptr, info_ptr);
-	out->height   = png_get_image_height (png_ptr, info_ptr);
-	out->channels = png_get_channels     (png_ptr, info_ptr);
+	png_read_update_info(state->pngp, state->infop);
+	out->width    = png_get_image_width  (state->pngp, state->infop);
+	out->height   = png_get_image_height (state->pngp, state->infop);
+	out->channels = png_get_channels     (state->pngp, state->infop);
 
 	// Allocate the image data.
-	if ((out->buf = malloc(out->width * out->height * out->channels)) == NULL) {
-		err = true;
-		goto exit;
-	}
+	if ((out->buf = malloc(out->width * out->height * out->channels)) == NULL)
+		return false;
 
 	// Allocate array of row pointers, read actual image data:
 	// TODO: check if it's wise to allocate this on the stack:
@@ -155,17 +161,38 @@ png_load (const struct png_in *in, struct png_out *out)
 			row_pointers[i] = b;
 			b += row_stride;
 		}
-		png_read_image(png_ptr, row_pointers);
+		png_read_image(state->pngp, row_pointers);
 	}
-	png_read_end(png_ptr, NULL);
+	png_read_end(state->pngp, NULL);
 
-	// Cleanup:
-exit:	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+	return true;
+}
 
-	if (!err)
-		return true;
+bool
+png_load (const struct png_in *in, struct png_out *out)
+{
+	bool ret;
+	struct state state;
 
-	free(out->buf);
-	out->buf = NULL;
-	return false;
+	// Initialize the state structure.
+	if (init(&state, in, out) == false)
+		return false;
+
+	// Return here on errors.
+	if (setjmp(png_jmpbuf(state.pngp))) {
+		ret = false;
+	} else {
+		ret = load(&state, out);
+	}
+
+	// Deinitialize the state structure.
+	deinit(&state);
+
+	// Free any allocated memory on an unsuccessful return.
+	if (ret == false) {
+		free(out->buf);
+		out->buf = NULL;
+	}
+
+	return ret;
 }
